bf669824e42581acab326fdbc84b40b9deacc606
victoria2588 <123986940+victoria2588@users.noreply.github.com>
2023-06-18 21:23:06 +0300

    task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('.feedback-form');
+const formInput = document.querySelector('.feedback-form input');
+const formTextarea = document.querySelector('.feedback-form textarea');
+const formData = {};
+
+outputForm();
+
+formEl.addEventListener('submit', onFormSubmit);
+
+formEl.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  event.currentTarget.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function onFormInput(event) {
+  formData[event.target.name] = event.target.value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function outputForm() {
+  const savedForm = localStorage.getItem('feedback-form-state');
+  const parsedForm = JSON.parse(savedForm);
+
+  if (parsedForm) {
+    formInput.value = parsedForm.email || '';
+    formTextarea.value = parsedForm.message || '';
+    console.log(parsedForm);
+  }
+}
